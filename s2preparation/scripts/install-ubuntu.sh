f994ef7b0154800d6db7719b72ddd315cf9eab37
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-05-02 14:10:12 +0200

    Fixing dependencies.

--- s2preparation/scripts/install-ubuntu.sh
+++ s2preparation/scripts/install-ubuntu.sh
@@ -3,7 +3,7 @@ apt-get install -y software-properties-common --no-install-recommends
 apt-add-repository ppa:ubuntugis/ubuntugis-unstable
 apt-get update
 apt-get install -y \
-    python-pip git wget \
+    python-pip python-lxml wget \
     python-gdal gdal-bin libgeos-dev libpython-dev libproj-dev python-pyproj python-shapely
 
 pip install -r requirements.txt
